1812a85512c57a12d264175c769b25865f4dcb62
sferna1 <santhosh.fernandes@wal-mart.com>
2023-05-24 00:32:17 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cilium/ebpf"
+	tc "github.com/florianl/go-tc"
+	"github.com/florianl/go-tc/core"
+	"golang.org/x/sys/unix"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	tcIface := "enp0s3"
+	devID, err := net.InterfaceByName(tcIface)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get interface ID: %v\n", err)
+		return
+	}
+
+	tcgo, err := tc.Open(&tc.Config{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open rtnetlink socket: %v\n", err)
+		return
+	}
+
+	// get all the qdiscs from all interfaces
+	qdiscs, err := tcgo.Qdisc().Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get qdiscs: %v\n", err)
+		return
+	}
+	clsactFound := false
+	for _, qdisc := range qdiscs {
+		iface, err := net.InterfaceByIndex(int(qdisc.Ifindex))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not get interface from id %d: %v", qdisc.Ifindex, err)
+			return
+		}
+		if iface.Name == tcIface && qdisc.Kind == "clsact" {
+			clsactFound = true
+		}
+	}
+
+	if !clsactFound {
+		qdisc := tc.Object{
+			Msg: tc.Msg{
+				Family:  unix.AF_UNSPEC,
+				Ifindex: uint32(devID.Index),
+				Handle:  core.BuildHandle(tc.HandleRoot, 0x0000),
+				Parent:  tc.HandleIngress,
+				Info:    0,
+			},
+			Attribute: tc.Attribute{
+				Kind: "clsact",
+			},
+		}
+
+		if err := tcgo.Qdisc().Add(&qdisc); err != nil {
+			fmt.Printf("could not assign clsact to %s: %v, its already exists", tcIface, err)
+		}
+	}
+
+	kernFile := "tc_program_kern.o"
+
+	prg, err := ebpf.LoadCollection(kernFile)
+	if err != nil {
+		fmt.Println("LoadCollection error : ", err)
+		return
+	}
+	defer prg.Close()
+
+	bpfProg := prg.Programs["tc_program"]
+	fd := uint32(bpfProg.FD())
+	flags := uint32(0x1)
+
+	filter := tc.Object{
+		tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(devID.Index),
+			Handle:  0,
+			Parent:  core.BuildHandle(tc.HandleRoot, tc.HandleMinIngress),
+			Info:    0x300,
+		},
+		tc.Attribute{
+			Kind: "bpf",
+			BPF: &tc.Bpf{
+				FD:    &fd,
+				Flags: &flags,
+			},
+		},
+	}
+	if err := tcgo.Filter().Add(&filter); err != nil {
+		fmt.Fprintf(os.Stderr, "could not attach filter for eBPF program: %v\n", err)
+		return
+	}
+
+	fmt.Println("Attaching tc program is successful, resting ...")
+	time.Sleep(2 * time.Second)
+
+	if err := tcgo.Filter().Delete(&filter); err != nil {
+		fmt.Fprintf(os.Stderr, "could not del filter for eBPF program: %v\n", err)
+		return
+	}
+}
---
+++ tc_program_kern.c
@@ -0,0 +1,30 @@
+// Copyright Contributors to the L3AF Project.
+// SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause)
+
+#define KBUILD_MODNAME "foo"
+
+#include <uapi/linux/bpf.h>
+#include "tc_root_common.h"
+
+#define bpf_printk(fmt, ...)                                    \
+({                                                              \
+               char ____fmt[] = fmt;                            \
+               bpf_trace_printk(____fmt, sizeof(____fmt),       \
+                                ##__VA_ARGS__);                 \
+})
+                  
+struct {
+        __uint(type, BPF_MAP_TYPE_PROG_ARRAY);
+        __type(key, int);
+        __type(value, int);
+        __uint(max_entries, ROOT_ARRAY_SIZE);
+} tc_ingress_root_array SEC(".maps");
+                  
+SEC("tc-program")
+int tc_program(struct __sk_buff *skb) {
+    bpf_tail_call(skb, &tc_ingress_root_array, 0);
+    return TC_ACT_OK;
+}
+
+
+char _license[] SEC("license") = "Dual BSD/GPL";
